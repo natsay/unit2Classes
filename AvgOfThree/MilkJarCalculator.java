b4af1769b40533c828243ab3550f02ae355da60a
natsay <nhtse@NN123-HA-12.HS.sd203.org>
2015-09-25 14:12:54 -0500

    9/25/15

---
+++ AvgOfThree/MilkJarCalculator.java
@@ -0,0 +1,11 @@
+public class MilkJarCalculator
+{
+   public static void main(String[] args)
+   {
+      double PMT= 10,000;
+      double i= .08; 
+      double n= 20; 
+      
+     double PVann=PMT((Math.1+i),pow
+   }
+}
